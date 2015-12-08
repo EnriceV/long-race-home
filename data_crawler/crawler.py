47f89567d01d5f1af5e6b517050cc087ac2a5453
bartaelterman <bart.aelterman@gmail.com>
2015-12-08 09:34:46 +0100

    update crawler

--- data_crawler/crawler.py
+++ data_crawler/crawler.py
@@ -49,21 +49,28 @@ class TracksCrawler():
         if self.DEBUG:
             self._generate_random_riders()
         else:
-            self.riders = [{
-                'full_name': 'test',
-                'tracker_id': 'test'
-            }]
-            raise Exception('Not implemented yet')
+            sql = 'SELECT * FROM riders'
+            params = {'q': sql, 'api_key':self.cartodbAPI}
+            r = requests.get('https://bartaelterman.cartodb.com/api/v2/sql', params=params)
+            data = r.json()['rows']
+            self.riders = data
 
-    def _fetch_messages_for_tracker(self, rider):
+
+    def _fetch_messages_for_rider(self, rider):
         messages = []
         if self.DEBUG:
             for i in range(10):
                 messages.append(self._generate_random_message(rider['full_name']))
         else:
-            r = requests.get(self.URL.format(tracker_id=rider['tracker_id']))
-            data = r.json()
-            messages = data['response']['feedMessageResponse']['messages']['message']
+            url = self.URL.format(tracker_id=rider['tracker_id'])
+            print url
+            r = requests.get(url)
+            if r.status_code == 200:
+                data = r.json()
+                if u'errors' in data['response'].keys():
+                    pprint.pprint(data['response']['errors'])
+                else:
+                    messages = data['response']['feedMessageResponse']['messages']['message']
         return messages
 
     def _existing_message_ids(self):
@@ -74,31 +81,45 @@ class TracksCrawler():
         if self.DEBUG:
             return [1, 2, 3]
         else:
-            raise Exception('Not implemented yet.')
+            sql = 'SELECT message_id FROM tracks;'
+            params = {'q': sql, 'api_key':self.cartodbAPI}
+            r = requests.get('https://bartaelterman.cartodb.com/api/v2/sql', params=params)
+            data = r.json()['rows']
+            return [x['message_id'] for x in data]
 
-    def _write_messages_to_cartodb(self, messages):
+
+    def _write_messages_to_cartodb(self, message):
         """
         Write the incoming messages to the Cartodb table.
         :param messages: list of incoming messages
         :return:
         """
         if not self.DEBUG:
-            raise Exception('Not implemented yet.')
+            sql = """INSERT INTO tracks
+                (message_id, battery_state, date_time, latitude, longitude, rider_full_name, the_geom)
+                VALUES ({0}, '{1}', '{2}', {3}, {4}, '{5}', ST_SetSRID(ST_Point({4}, {3}),4326))""".format(
+                message['id'], message['batteryState'], message['dateTime'], message['latitude'],
+                       message['longitude'], message['rider_name'])
+            print sql
+            params = {'q': sql, 'api_key':self.cartodbAPI}
+            r = requests.get('https://bartaelterman.cartodb.com/api/v2/sql', params=params)
+            print('API_RESPONSE: {0}: {1}: {2}'.format(message['rider_name'], r.status_code, r.text))
+
 
     def fetch_new_messages(self):
         existing_messages = self._existing_message_ids()
         messages_to_write = []
         for rider in self.riders:
-            messages = self._fetch_messages_for_tracker(rider)
+            messages = self._fetch_messages_for_rider(rider)
             for message in messages:
                 if message['id'] not in existing_messages:
-                    message['rider'] = rider['full_name']
+                    message['rider_name'] = rider['full_name']
                     messages_to_write.append(message)
-        self._write_messages_to_cartodb(messages_to_write)
+                    self._write_messages_to_cartodb(message)
         if self.DEBUG:
             return messages_to_write
 
 if __name__ == '__main__':
-    crawler = TracksCrawler('', debug=True)
+    crawler = TracksCrawler('a72c682f541f69035c0698dacd029d3d19409aa6', debug=False)
     msgs = crawler.fetch_new_messages()
     pprint.pprint(msgs)
